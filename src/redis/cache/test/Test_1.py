75a6cc8e634623484619f47015ef7ed198dcc2bc
goldfix <decpack@goldfix.it>
2014-04-30 10:25:58 +0200

    Init Project

---
+++ src/redis/cache/test/Test_1.py
@@ -0,0 +1,21 @@
+'''
+Created on 30/apr/2014
+
+@author: ppartes-v
+'''
+
+
+class ClassTest_1(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+
+    def Test_1(self):
+        
+        pass
